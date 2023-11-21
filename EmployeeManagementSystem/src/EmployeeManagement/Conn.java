9b43891c4eedaafbfc261cc1c2a30d1628556b21
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2023-11-21 23:37:38 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/src/EmployeeManagement/Conn.java
@@ -0,0 +1,19 @@
+package EmployeeManagement;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "siemensproject");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
